c4e49dd8672ac6befb4633ac2674ee38a0cd1399
toretto323 <howarc91@gmail.com>
2025-08-17 18:07:09 -0400

    Update Package.swift
    first edit

--- Package.swift
+++ Package.swift
@@ -19,4 +19,20 @@ let package = Package(
             checksum: "5d2ef5d4fabcc29a0c32eebfd6c6093e37798b583e1c797f04dc3125ad6dbcf3"
         ),
     ]
+let package = Package(
+    // ...
+    dependencies: [
+        //// Option 1: Fetch latest version available. This allows automatic major version updates that may contain non-backward compatible changes.
+        .package(url: "https://github.com/microsoft/clarity-apps", branch: "main")
+        //// Option 2: Fetch a specific minor version range (e.g., 3.0.0 up to, but not including, 4.0.0).
+        // .package(url: "https://github.com/microsoft/clarity-apps", from: "3.0.0")
+    ],
+    targets: [
+        .target(
+            name: "<target-name>",
+            dependencies: [
+                .product(name: "Clarity", package: "clarity-apps")
+            ]
+        )
+    ]
 )
